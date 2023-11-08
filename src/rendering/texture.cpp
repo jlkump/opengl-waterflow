88ac3a92d943ad8e994448114a6d6c9a7d417632
Jonathan Kump <landon2002@gmail.com>
2023-11-08 13:32:04 -0600

    Adding texture modifications, can't build yet. WIP

--- src/rendering/texture.cpp
+++ src/rendering/texture.cpp
@@ -18,160 +18,258 @@
 ///	Public Methods ///
 //////////////////////
 
-Texture::Texture(int dimensions, GLenum desired_channels, GLenum storage_type, const float* data) 
-    : texture_id_(0), channels_(desired_channels), storage_type_(storage_type), valid_texture_(true), dimensions_(dimensions)
+//Texture::Texture(int dimensions, GLenum desired_channels, GLenum storage_type, const float* data) 
+//    : texture_id_(0), channels_(desired_channels), storage_type_(storage_type), valid_texture_(true), dimensions_(dimensions)
+//{
+//    if (desired_channels < GL_RED || desired_channels > GL_RGBA) 
+//    {
+//        valid_texture_ = false;
+//        fprintf(stderr, "Texture created with %d number of channels instead of between 1 and 4\n", desired_channels);
+//        return;
+//    }
+//    glGenTextures(1, &texture_id_);
+//    glBindTexture(GL_TEXTURE_2D, texture_id_);
+//
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+//
+//    switch (desired_channels) {
+//    case GL_RED:
+//        glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, dimensions, dimensions, 0, GL_RED, GL_FLOAT, data);
+//        break;
+//    case GL_RG:
+//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RG32F, dimensions, dimensions, 0, GL_RG, GL_FLOAT, data);
+//        break;
+//    case GL_RGB:
+//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, dimensions, dimensions, 0, GL_RGB, GL_FLOAT, data);
+//        break;
+//    case GL_RGBA:
+//        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, dimensions, dimensions, 0, GL_RGBA, GL_FLOAT, data);
+//        break;
+//    };
+//}
+//
+//Texture::Texture(glm::ivec2 dimensions, GLenum desired_channels, GLenum storage_type, const float* data)
+//    : texture_id_(0), channels_(desired_channels), storage_type_(storage_type), valid_texture_(true), dimensions_(dimensions)
+//{
+//    if (desired_channels < GL_RED || desired_channels > GL_RGBA)
+//    {
+//        valid_texture_ = false;
+//        fprintf(stderr, "Texture created with %d number of channels instead of between GL_RED and GL_RGBA\n", desired_channels);
+//        return;
+//    }
+//    glGenTextures(1, &texture_id_);
+//    glBindTexture(GL_TEXTURE_2D, texture_id_);
+//
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+//
+//    if (storage_type == GL_R32F || storage_type == GL_RG32F || storage_type == GL_RGB32F || storage_type == GL_RGBA32F) 
+//    {
+//        glTexImage2D(GL_TEXTURE_2D, 0, storage_type, dimensions.x, dimensions.y, 0, desired_channels, GL_FLOAT, data);
+//    }
+//    else 
+//    {
+//        glTexImage2D(GL_TEXTURE_2D, 0, storage_type, dimensions.x, dimensions.y, 0, desired_channels, GL_UNSIGNED_BYTE, data);
+//    }
+//}
+//
+//Texture::Texture(const std::string& filename) 
+//    : texture_id_(0), channels_(GL_RGBA), storage_type_(GL_RGBA8), valid_texture_(true), dimensions_(0)
+//{
+//    if (filename.empty())
+//    {
+//        valid_texture_ = false;
+//        fprintf(stderr, "Failure creating Texture obj, file name provided is empty.\n");
+//        return;
+//    }
+//
+//    bool is_loaded = false;
+//    int width, height;
+//
+//    unsigned char* pixels = stbi_load((ROOT_DIR "resources/textures/" + filename).c_str(), &width, &height, nullptr, 4);
+//
+//    dimensions_.x = width;
+//    dimensions_.y = height;
+//
+//    if (pixels != nullptr)
+//    {
+//        glGenTextures(1, &texture_id_);
+//        glBindTexture(GL_TEXTURE_2D, texture_id_);
+//
+//        glTexStorage2D(GL_TEXTURE_2D, 2 /* mip map levels */, GL_RGBA8, width, height);
+//        glTexSubImage2D(GL_TEXTURE_2D, 0 /* mip map level */, 0 /* xoffset */, 0 /* yoffset */, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+//        glGenerateMipmap(GL_TEXTURE_2D); // Replaces mipmap levels from 0 to 2 with generated mip maps
+//
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+//    }
+//    else
+//    {
+//        valid_texture_ = false;
+//        fprintf(stderr, "Texture \"%s\" could not be loaded. Is the texture file in \"resources/textures/\"?\n", filename.c_str());
+//    }
+//
+//    stbi_image_free(pixels);
+//}
+//
+//Texture::~Texture() {
+//    if (texture_id_ != 0)
+//    {
+//        glDeleteTextures(1, &texture_id_);
+//    }
+//}
+//
+//void Texture::ActiveBind(GLenum texture_unit)
+//{
+//    if (!valid_texture_)
+//    {
+//        return;
+//    }
+//    glActiveTexture(texture_unit);
+//    glBindTexture(GL_TEXTURE_2D, texture_id_);
+//}
+//
+//void Texture::BindImage(unsigned int binding) 
+//{
+//    if (!valid_texture_) 
+//    {
+//        return;
+//    }
+//    glBindImageTexture(binding, texture_id_, 0, 0, 0, GL_READ_WRITE, storage_type_);
+//}
+//
+//GLuint Texture::GetTextureId() 
+//{
+//    return texture_id_;
+//}
+//
+//void Texture::UpdatePixelData(GLint x_offset, GLint y_offset, GLsizei width, GLsizei height, const void* pixel_data)
+//{
+//    if (!valid_texture_) 
+//    {
+//        return;
+//    }
+//    glBindTexture(GL_TEXTURE_2D, texture_id_);
+//    glPixelStorei(GL_UNPACK_ROW_LENGTH, width);
+//    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+//    if (storage_type_ == GL_R8 || storage_type_ == GL_RG8 || storage_type_ == GL_RGB8 || storage_type_ == GL_RGBA8) 
+//    {
+//        glTexSubImage2D(GL_TEXTURE_2D, 0, x_offset, y_offset, width, height, channels_, GL_BYTE, pixel_data);
+//    }
+//    else 
+//    {
+//        glTexSubImage2D(GL_TEXTURE_2D, 0, x_offset, y_offset, width, height, channels_, GL_FLOAT, pixel_data);
+//    }
+//    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+//    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+//}
+//
+//glm::ivec2 Texture::GetDimensions()
+//{
+//    return dimensions_;
+//}
+
+using namespace Texture;
+
+Texture2D::Texture2D(glm::ivec2 dimensions, StorageType storage_type, ChannelType channel_type, const void* initial_data) :
+	gl_storage_type_(GL_NONE), gl_channel_type_(GL_NONE), valid_texture_(false)
 {
-    if (desired_channels < GL_RED || desired_channels > GL_RGBA) 
-    {
-        valid_texture_ = false;
-        fprintf(stderr, "Texture created with %d number of channels instead of between 1 and 4\n", desired_channels);
-        return;
-    }
-    glGenTextures(1, &texture_id_);
+	// Generate handle for texture on GPU
+	glGenTextures(1, &texture_id_);
+	// Bind the texture to the active texture. Future texture calls modify this texture
+	glBindTexture(GL_TEXTURE_2D, texture_id_);
+
+	// Set texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+	switch (channel_type) {
+		case ChannelType::R:
+			gl_channel_type_ = GL_RED;
+			break;
+		case ChannelType::RG:
+			gl_channel_type_ = GL_RG;
+			break;
+		case ChannelType::RGB:
+			gl_channel_type_ = GL_RGB;
+			break;
+		case ChannelType::RGBA:
+			gl_channel_type_ = GL_RGBA;
+			break;
+	}
+
+	switch (storage_type) {
+		case StorageType::BYTE:
+			gl_storage_type_ = GL_BYTE;
+			break;
+		case StorageType::SHORT:
+			gl_storage_type_ = GL_SHORT;
+			break;
+		case StorageType::INT:
+			gl_storage_type_ = GL_INT;
+			break;
+		case StorageType::FLOAT:
+			gl_storage_type_ = GL_FLOAT;
+			break;
+	}
+	// Place texture data to the GPU
+	glTexImage2D(GL_TEXTURE_2D, 0, gl_channel_type_, dimensions.x, dimensions.y, 0, gl_channel_type_, gl_storage_type_, initial_data);
+	// TODO: Error check the result of OpenGL calls
+	valid_texture_ = true;
+}
+
+bool Texture2D::ModifyTextureData(glm::ivec2 top_left_start, glm::ivec2 data_dimensions, const void* texture_data)
+{
+	if (!valid_texture_) {
+		return false;
+	}
     glBindTexture(GL_TEXTURE_2D, texture_id_);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-    switch (desired_channels) {
-    case GL_RED:
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, dimensions, dimensions, 0, GL_RED, GL_FLOAT, data);
-        break;
-    case GL_RG:
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RG32F, dimensions, dimensions, 0, GL_RG, GL_FLOAT, data);
-        break;
-    case GL_RGB:
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, dimensions, dimensions, 0, GL_RGB, GL_FLOAT, data);
-        break;
-    case GL_RGBA:
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, dimensions, dimensions, 0, GL_RGBA, GL_FLOAT, data);
-        break;
-    };
-}
-
-Texture::Texture(glm::ivec2 dimensions, GLenum desired_channels, GLenum storage_type, const float* data)
-    : texture_id_(0), channels_(desired_channels), storage_type_(storage_type), valid_texture_(true), dimensions_(dimensions)
-{
-    if (desired_channels < GL_RED || desired_channels > GL_RGBA)
-    {
-        valid_texture_ = false;
-        fprintf(stderr, "Texture created with %d number of channels instead of between GL_RED and GL_RGBA\n", desired_channels);
-        return;
-    }
-    glGenTextures(1, &texture_id_);
-    glBindTexture(GL_TEXTURE_2D, texture_id_);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-    if (storage_type == GL_R32F || storage_type == GL_RG32F || storage_type == GL_RGB32F || storage_type == GL_RGBA32F) 
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, storage_type, dimensions.x, dimensions.y, 0, desired_channels, GL_FLOAT, data);
-    }
-    else 
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, storage_type, dimensions.x, dimensions.y, 0, desired_channels, GL_UNSIGNED_BYTE, data);
-    }
-}
-
-Texture::Texture(const std::string& filename) 
-    : texture_id_(0), channels_(GL_RGBA), storage_type_(GL_RGBA8), valid_texture_(true), dimensions_(0)
-{
-    if (filename.empty())
-    {
-        valid_texture_ = false;
-        fprintf(stderr, "Failure creating Texture obj, file name provided is empty.\n");
-        return;
-    }
-
-    bool is_loaded = false;
-    int width, height;
-
-    unsigned char* pixels = stbi_load((ROOT_DIR "resources/textures/" + filename).c_str(), &width, &height, nullptr, 4);
-
-    dimensions_.x = width;
-    dimensions_.y = height;
-
-    if (pixels != nullptr)
-    {
-        glGenTextures(1, &texture_id_);
-        glBindTexture(GL_TEXTURE_2D, texture_id_);
-
-        glTexStorage2D(GL_TEXTURE_2D, 2 /* mip map levels */, GL_RGBA8, width, height);
-        glTexSubImage2D(GL_TEXTURE_2D, 0 /* mip map level */, 0 /* xoffset */, 0 /* yoffset */, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-        glGenerateMipmap(GL_TEXTURE_2D); // Replaces mipmap levels from 0 to 2 with generated mip maps
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    }
-    else
-    {
-        valid_texture_ = false;
-        fprintf(stderr, "Texture \"%s\" could not be loaded. Is the texture file in \"resources/textures/\"?\n", filename.c_str());
-    }
-
-    stbi_image_free(pixels);
-}
-
-Texture::~Texture() {
-    if (texture_id_ != 0)
-    {
-        glDeleteTextures(1, &texture_id_);
-    }
-}
-
-void Texture::ActiveBind(GLenum texture_unit)
-{
-    if (!valid_texture_)
-    {
-        return;
-    }
-    glActiveTexture(texture_unit);
-    glBindTexture(GL_TEXTURE_2D, texture_id_);
-}
-
-void Texture::BindImage(unsigned int binding) 
-{
-    if (!valid_texture_) 
-    {
-        return;
-    }
-    glBindImageTexture(binding, texture_id_, 0, 0, 0, GL_READ_WRITE, storage_type_);
-}
-
-GLuint Texture::GetTextureId() 
-{
-    return texture_id_;
-}
-
-void Texture::UpdatePixelData(GLint x_offset, GLint y_offset, GLsizei width, GLsizei height, const void* pixel_data)
-{
-    if (!valid_texture_) 
-    {
-        return;
-    }
-    glBindTexture(GL_TEXTURE_2D, texture_id_);
-    glPixelStorei(GL_UNPACK_ROW_LENGTH, width);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, data_dimensions.x);
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    if (storage_type_ == GL_R8 || storage_type_ == GL_RG8 || storage_type_ == GL_RGB8 || storage_type_ == GL_RGBA8) 
-    {
-        glTexSubImage2D(GL_TEXTURE_2D, 0, x_offset, y_offset, width, height, channels_, GL_BYTE, pixel_data);
-    }
-    else 
-    {
-        glTexSubImage2D(GL_TEXTURE_2D, 0, x_offset, y_offset, width, height, channels_, GL_FLOAT, pixel_data);
-    }
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+	glTexSubImage2D(GL_TEXTURE_2D, 0, top_left_start.x, top_left_start.y, data_dimensions.x, data_dimensions.y, gl_channel_type_, gl_storage_type_, texture_data);
+
+	return true;
 }
 
-glm::ivec2 Texture::GetDimensions()
+glm::ivec2 Texture2D::GetTextureDimensions()
 {
-    return dimensions_;
+	return dimensions_;
+}
+
+GLenum Texture2D::GetGLStorageType()
+{
+	return storage_type_;
+}
+
+GLenum Texture2D::GetGLChannelType()
+{
+	return channel_type_;
+}
+
+bool Texture2D::ActiveBind(GLenum texture_unit_binding)
+{
+	if (!valid_texture_) {
+		return false;
+	}
+    glActiveTexture(texture_unit_binding);
+    glBindTexture(GL_TEXTURE_2D, texture_id_);
+	return true;
+}
+
+bool Texture2D::BindImageTexture(GLenum texture_unit_binding)
+{
+	if (!valid_texture_) {
+		return false;
+	}
+	glBindImageTexture(texture_unit_binding, texture_id_, 0, 0, 0, GL_READ_WRITE, gl_storage_type_);
+	return true;
 }
--- src/rendering/texture.hpp
+++ src/rendering/texture.hpp
@@ -5,127 +5,181 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 
-class Texture 
-{
-protected:
-	/*
-	* @brief
-	* Tracks the texture created on the GPU with a texture id
-	* Manipulating the texture on the GPU will require this id
-	*/
+namespace Texture {
+	enum StorageType {
+		FLOAT,
+		BYTE,
+		SHORT,
+		INT,
+	};
+
+	enum ChannelType {
+		R,
+		RG,
+		RGB,
+		RGBA,
+	};
+};
+
+
+class Texture2D {
+private:
 	GLuint texture_id_;
-
-	/*
-	* @brief
-	* The number of color channels for this texture,
-	* ranging from 1 to 4 (R, RG, RGB, RGBA)
-	*/
-	GLenum channels_;
-
-	/*
-	* @brief
-	* The storage type for this texture, whether it be 0-255 int rgbs or float 0-1.0
-	*/
-	GLenum storage_type_;
-
-	/*
-	* @brief
-	* A simple bool to check that the texture is valid or not.
-	* This prevents the texture from being used if there was a problem
-	* loading in the images.
-	*/
-	bool valid_texture_;
-
+	Texture::StorageType storage_type_;
+	GLenum gl_storage_type_;
+	Texture::ChannelType channel_type_;
+	GLenum gl_channel_type_;
 	glm::ivec2 dimensions_;
 
-	Texture() : texture_id_(0), channels_(GL_RGB), storage_type_(GL_RGB8), valid_texture_(true), dimensions_(glm::ivec2(0)) {}
-
+	bool valid_texture_;
+	
 public:
-	/*
-	* @brief
-	* Creates an empty texture which can be useful for drawing
-	* to texture with shaders. In this case, the GetTextureId()
-	* method will be useful in binding the texture to the
-	* frame buffer.
-	* 
-	* 
-	* @ param 
-	* dimensions: The dimensions of the texture. Note: This
-	* must be a power of 2, otherwise there will be problems
-	* with loading the texture into GPU memory.
-	* 
-	* @param
-	* desired_channels:
-	* The desired number of RGBA channels for the texture. Defaults to RGBA
-	* Specified using: GL_RED, GL_RG, GL_RGB, or GL_RGBA
-	* 
-	* @param
-	* storage type:
-	* The way the color for this texture is represented, either as a float or as a byte (0 to 255).
-	* Specified using: GL_R32F, ... GL_RGBA32F or GL_R8 ... GL_RGBA8
-	* 
-	* @param
-	* data:
-	* The starting data for this texture to be loaded with. Must match the type of the given storage type.
-	* Ex: A choice of RGBA32F (RGBA as floats) must have data of [0.2, 0.1, 0.9, 1.0, ...] with each set of
-	* four floats representing a color.
-	*/
-	Texture(int dimensions, GLenum desired_channels = GL_RGBA, GLenum storage_type = GL_RGBA32F, const float* data = nullptr);
-	/*
-	* Same as standard texture, except dimensions defines width and height rather than
-	* just box dimensions. Must be a power of 2 for dimension sizes
-	*/
-	Texture(glm::ivec2 dimensions, GLenum desired_channels = GL_RGBA, GLenum storage_type = GL_RGBA32F, const float* data = nullptr);
+	Texture2D(glm::ivec2 dimensions, Texture::StorageType storage_type = Texture::StorageType::FLOAT, 
+		Texture::ChannelType channel_type = Texture::ChannelType::RGBA, const void* initial_data);
 
-	/*
-	* Loads in a texture with the given filename.
-	* The texture is assumed to be in "resources/textures/"
-	* and the call will fail if the texture is located elsewhere.
-	* 
-	* If the texture is in a subdirectory in texture, pass in
-	* as "subdirectory/texture.png"
-	* 
-	* @param
-	* filename:
-	* The name of the file. The path is assumed and thus
-	* the texture must be in "resources/textures/"
-	*/
-	Texture(const std::string& filename);
-	~Texture();
-
-	/*
-	* Calls glActiveTexture and glBindTexture in sequence,
-	* which will set this texture as the active
-	* texture for future texture based OpenGL calls.
-	* Typically called in the for-loop for rendering.
-	* 
-	* @param
-	* texture_unit: A texture_unit must be provided which is
-	* associated with the location where to bind
-	* the texture in the shader program. Default = GL_TEXTURE0
-	* Example: GL_TEXTURE0, GL_TEXTURE1, etc.
-	* 
-	* 
-	*/
-	virtual void ActiveBind(GLenum texture_unit = GL_TEXTURE0);
-
-	void BindImage(unsigned int binding);
 
 	/*
 	* Returns the texture ID associated with this texture.
 	* Useful for binding a texture to the FrameBuffer or
 	* performing other modifications to the texture.
-	* 
+	*
 	* Example of rendering to texture:
 	* > // Set "renderedTexture" as our colour attachement #0
 	* > glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, Texture.GetTextureId(), 0);
-	* 
+	*
 	*/
 	GLuint GetTextureId();
 
-	void UpdatePixelData(GLint x_offset, GLint y_offset, GLsizei width, GLsizei height, const void* pixel_data);
+	bool ModifyTextureData(glm::ivec2 top_left_start, glm::ivec2 data_dimensions, const void* texture_data);
+	glm::ivec2 GetTextureDimensions();
+	GLenum GetGLStorageType();
+	GLenum GetGLChannelType();
 
-	glm::ivec2 GetDimensions();
+	virtual bool ActiveBind(GLenum texture_unit_binding);
+	virtual bool BindImageTexture(GLenum texture_unit_binding);
 };
+
+//class Texture 
+//{
+//protected:
+//	/*
+//	* @brief
+//	* Tracks the texture created on the GPU with a texture id
+//	* Manipulating the texture on the GPU will require this id
+//	*/
+//	GLuint texture_id_;
+//
+//	/*
+//	* @brief
+//	* The number of color channels for this texture,
+//	* ranging from 1 to 4 (R, RG, RGB, RGBA)
+//	*/
+//	GLenum channels_;
+//
+//	/*
+//	* @brief
+//	* The storage type for this texture, whether it be 0-255 int rgbs or float 0-1.0
+//	*/
+//	GLenum storage_type_;
+//
+//	/*
+//	* @brief
+//	* A simple bool to check that the texture is valid or not.
+//	* This prevents the texture from being used if there was a problem
+//	* loading in the images.
+//	*/
+//	bool valid_texture_;
+//
+//	glm::ivec2 dimensions_;
+//
+//	Texture() : texture_id_(0), channels_(GL_RGB), storage_type_(GL_RGB8), valid_texture_(true), dimensions_(glm::ivec2(0)) {}
+//
+//public:
+//	/*
+//	* @brief
+//	* Creates an empty texture which can be useful for drawing
+//	* to texture with shaders. In this case, the GetTextureId()
+//	* method will be useful in binding the texture to the
+//	* frame buffer.
+//	* 
+//	* 
+//	* @ param 
+//	* dimensions: The dimensions of the texture. Note: This
+//	* must be a power of 2, otherwise there will be problems
+//	* with loading the texture into GPU memory.
+//	* 
+//	* @param
+//	* desired_channels:
+//	* The desired number of RGBA channels for the texture. Defaults to RGBA
+//	* Specified using: GL_RED, GL_RG, GL_RGB, or GL_RGBA
+//	* 
+//	* @param
+//	* storage type:
+//	* The way the color for this texture is represented, either as a float or as a byte (0 to 255).
+//	* Specified using: GL_R32F, ... GL_RGBA32F or GL_R8 ... GL_RGBA8
+//	* 
+//	* @param
+//	* data:
+//	* The starting data for this texture to be loaded with. Must match the type of the given storage type.
+//	* Ex: A choice of RGBA32F (RGBA as floats) must have data of [0.2, 0.1, 0.9, 1.0, ...] with each set of
+//	* four floats representing a color.
+//	*/
+//	Texture(int dimensions, GLenum desired_channels = GL_RGBA, GLenum storage_type = GL_RGBA32F, const float* data = nullptr);
+//	/*
+//	* Same as standard texture, except dimensions defines width and height rather than
+//	* just box dimensions. Must be a power of 2 for dimension sizes
+//	*/
+//	Texture(glm::ivec2 dimensions, GLenum desired_channels = GL_RGBA, GLenum storage_type = GL_RGBA32F, const float* data = nullptr);
+//
+//	/*
+//	* Loads in a texture with the given filename.
+//	* The texture is assumed to be in "resources/textures/"
+//	* and the call will fail if the texture is located elsewhere.
+//	* 
+//	* If the texture is in a subdirectory in texture, pass in
+//	* as "subdirectory/texture.png"
+//	* 
+//	* @param
+//	* filename:
+//	* The name of the file. The path is assumed and thus
+//	* the texture must be in "resources/textures/"
+//	*/
+//	Texture(const std::string& filename);
+//	~Texture();
+//
+//	/*
+//	* Calls glActiveTexture and glBindTexture in sequence,
+//	* which will set this texture as the active
+//	* texture for future texture based OpenGL calls.
+//	* Typically called in the for-loop for rendering.
+//	* 
+//	* @param
+//	* texture_unit: A texture_unit must be provided which is
+//	* associated with the location where to bind
+//	* the texture in the shader program. Default = GL_TEXTURE0
+//	* Example: GL_TEXTURE0, GL_TEXTURE1, etc.
+//	* 
+//	* 
+//	*/
+//	virtual void ActiveBind(GLenum texture_unit = GL_TEXTURE0);
+//
+//	void BindImage(unsigned int binding);
+//
+//	/*
+//	* Returns the texture ID associated with this texture.
+//	* Useful for binding a texture to the FrameBuffer or
+//	* performing other modifications to the texture.
+//	* 
+//	* Example of rendering to texture:
+//	* > // Set "renderedTexture" as our colour attachement #0
+//	* > glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, Texture.GetTextureId(), 0);
+//	* 
+//	*/
+//	GLuint GetTextureId();
+//
+//	void UpdatePixelData(GLint x_offset, GLint y_offset, GLsizei width, GLsizei height, const void* pixel_data);
+//
+//	glm::ivec2 GetDimensions();
+//};
 
 #endif
